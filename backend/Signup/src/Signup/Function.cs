2a50bffd6944a6c9063f6a8f0f8ccb5eeb34a4cd
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-10-18 15:20:09 -0500

    Added signup logic

--- backend/Signup/src/Signup/Function.cs
+++ backend/Signup/src/Signup/Function.cs
@@ -1,4 +1,8 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using System.Net;
 using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -8,7 +12,19 @@ namespace Signup;
 
 public class Function
 {
-    
+    private const string userTblName = "user";
+    private const string profileImageS3BucketName = "profile-image-cs516";
+
+    private readonly IAmazonDynamoDB dynamoDbClient;
+    private readonly IAmazonS3 s3Client;
+    public Function()
+    {
+        this.dynamoDbClient = new AmazonDynamoDBClient();
+        this.s3Client = new AmazonS3Client();
+    }
+
+
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -19,10 +35,61 @@ public class Function
     {
         try
         {
+            var dbGetRequest = new GetItemRequest()
+            {
+                TableName = userTblName,
+                Key = new Dictionary<string, AttributeValue>
+                {
+                    { "email", new AttributeValue { S = input.email }  }
+                }
+            };
+
+            var existing = await dynamoDbClient.GetItemAsync(dbGetRequest);
+            if (existing.Item.Any())
+            {
+                throw new ApplicationException($"{input.email} already exists.");
+            }
+
+
+            var dbPutRequest = new PutItemRequest()
+            {
+                TableName = userTblName,
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    { "email", new AttributeValue { S = input.email } },
+                    { "password", new AttributeValue { S = BCrypt.Net.BCrypt.HashPassword(input.password) } },
+                    { "name", new AttributeValue { S = input.name } },
+                    { "profileImage", new AttributeValue { S = $"https://{profileImageS3BucketName}.s3.amazonaws.com/{input.email}" } }
+                }
+            };
+
+            var dbPutResult = await dynamoDbClient.PutItemAsync(dbPutRequest);
+            if (dbPutResult.HttpStatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Fail to insert record into database.");
+            }
+
+            var s3PreSignedUrl = s3Client.GetPreSignedURL(new Amazon.S3.Model.GetPreSignedUrlRequest()
+            {
+                BucketName = profileImageS3BucketName,
+                Key = input.email,
+                Expires = DateTime.UtcNow.AddSeconds(60)
+            });
+
+
             return new Response()
             {
                 status = true,
-                message = "OK" + JsonSerializer.Serialize(input)
+                message = "OK",
+                s3PreSignedUrl = s3PreSignedUrl
+            };
+        }
+        catch (ApplicationException ex)
+        {
+            return new Response()
+            {
+                status = false,
+                message = ex.Message
             };
         }
         catch (Exception ex)
